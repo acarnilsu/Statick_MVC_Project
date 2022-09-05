e2cc6552c30770b0c07bbde85ed8fc0f22d8a871
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-09-05 21:47:24 +0300

    Add project files.

---
+++ StatickMVC/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StatickMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
